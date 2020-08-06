0193586ee517ce613c33f4ef53d48fb5af150ef5
marco <marcodecrema@libero.it>
2020-08-06 19:37:32 +0200

    script finale

--- js/script.js
+++ js/script.js
@@ -1,15 +1,28 @@
+// Richiesta all'utente del nome
 var nome = prompt('Inserisci il tuo nome');
+// Richiesta all'utente del sesso
 var sesso = prompt('Inserisci il tuo sesso');
+// Elemento in cui verrà stampato il nome all'interno della pagina
 var nomeUtente = document.getElementById('nomeUtente');
+// Elemento html
 var ciao = document.getElementById('ciao');
 
 
+// Condizione iniziale per poter funzionare
+if (isNaN(nome)) {
+// Casistiche
 if (sesso == "uomo") {
+  nomeUtente.style.color="blue";
   ciao.style.display="block";
   nomeUtente.innerHTML=" "+nome;
 } else if (sesso == "donna") {
+  nomeUtente.style.color="pink"
   ciao.style.display="block";
   nomeUtente.innerHTML=" "+nome;
 } else {
   alert('Il sesso inserito è errato');
+}
+// Messaggio di errore
+} else {
+  alert('Devi inserire un nome valido');
 }
